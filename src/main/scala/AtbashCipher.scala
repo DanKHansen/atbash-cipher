0a3a63e140b92a6f1a8ff75666f9ceb06cd73537
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-28 11:55:30 +0200

    solution

--- src/main/scala/AtbashCipher.scala
+++ src/main/scala/AtbashCipher.scala
@@ -1,3 +1,7 @@
 object AtbashCipher:
-   def encode(s: String): String = ???
-   def decode(s: String): String = ???
+   private val mapping: Map[Char, Char] =
+      ('a' to 'z').map(c => (c, ('z' - (c - 'a')).toChar)).toMap ++ ('0' to '9').map(c => (c, c)).toMap
+
+   def encode(s: String): String =
+      s.toLowerCase.toCharArray.filter(_.isLetterOrDigit).map(mapping).mkString.grouped(5).mkString(" ")
+   def decode(s: String): String = s.toLowerCase.toCharArray.filter(_.isLetterOrDigit).map(mapping).mkString
