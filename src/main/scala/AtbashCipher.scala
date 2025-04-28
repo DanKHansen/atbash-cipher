d62174df86f7a1f1ba7261cab2db36e6f90edeb9
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-28 12:32:36 +0200

    refactoring

--- src/main/scala/AtbashCipher.scala
+++ src/main/scala/AtbashCipher.scala
@@ -1,7 +1,7 @@
 object AtbashCipher:
-   private def mapChar(c: Char): Char =
-      if c.isLetter then ('a' + ('z' - c)).toChar else c
+   private def streamLine(s: String): String =
+      s.toLowerCase.filter(_.isLetterOrDigit).map(c => if c.isLetter then ('a' + ('z' - c)).toChar else c)
    def encode(s: String): String =
-      decode(s).grouped(5).mkString(" ")
+      streamLine(s).grouped(5).mkString(" ")
    def decode(s: String): String =
-      s.toLowerCase.filter(_.isLetterOrDigit).map(mapChar).mkString
+      streamLine(s).mkString
