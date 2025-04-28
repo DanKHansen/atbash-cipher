d0226de833c3c378669ee3d70493e116ddebf0a6
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-28 11:59:42 +0200

    refactoring

--- src/main/scala/AtbashCipher.scala
+++ src/main/scala/AtbashCipher.scala
@@ -1,7 +1,6 @@
 object AtbashCipher:
    private val mapping: Map[Char, Char] =
-      ('a' to 'z').map(c => (c, ('z' - (c - 'a')).toChar)).toMap ++ ('0' to '9').map(c => (c, c)).toMap
+      ('a' to 'z').map(c => c -> ('z' - (c - 'a')).toChar).toMap ++ ('0' to '9').map(c => c -> c).toMap
 
-   def encode(s: String): String =
-      s.toLowerCase.toCharArray.filter(_.isLetterOrDigit).map(mapping).mkString.grouped(5).mkString(" ")
-   def decode(s: String): String = s.toLowerCase.toCharArray.filter(_.isLetterOrDigit).map(mapping).mkString
+   def encode(s: String): String = s.toLowerCase.filter(_.isLetterOrDigit).map(mapping).grouped(5).mkString(" ")
+   def decode(s: String): String = s.toLowerCase.filter(_.isLetterOrDigit).map(mapping).mkString
